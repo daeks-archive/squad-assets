7ad534f4de0c2446ca3664d4e8cf3b5f25367641
OleWarrink <Warrink.Ole@googlemail.com>
2023-10-07 03:00:12 +0200

    added tool for preping different files

---
+++ tools/README.md
@@ -0,0 +1,10 @@
+# Squad Assets Toolset
+
+These are tools to help with creation and updating of the Squad Assets
+
+# dataextractor
+
+This helps to extract data from the provided Squad Wiki Pipeline Tool and gives you preped data for the Squad Assets. For best results, use the Squad Wiki Pipeline Tool to extract the newest layer data from the game sdk files, before running this tool. 
+
+# Credits
+https://github.com/Squad-Wiki/squad-wiki-pipeline-map-data
---
+++ tools/dataextractor.py
@@ -0,0 +1,61 @@
+import json
+import os
+import sys
+import time
+import traceback
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def main():
+    # Get Data from LayerFile provided by SquadWikiPipeline Tool
+    layers: json = json.load(open(dir_path+"\\layers.json"))
+    maplistdata = createMapList(layers)
+    # save as maplist.txt
+    with open("maplist.txt", "w") as f:
+        for map in maplistdata:
+            f.write(map + "\n")
+
+
+
+def createMapList(layers:json)-> [str]:        # Create maplist.json data
+    result = []
+
+    for map in layers["Maps"]:
+        result.append(map["levelName"])
+    return result
+
+def createMapDataJSON(layers:json)-> json:        # Create mapdata.json data
+    result: json = {}
+
+    for map in layers["Maps"]:
+
+        teams = []    # Create teams array instead of using keys
+        for key in map:
+            if key.startswith("team"):
+                team_data = map[key]
+                team_data["name"] = key
+                #might add Team Tag here
+                teams.append(team_data)
+        
+
+
+        result[map["levelName"]] = {
+            "name": map["Name"],
+            "raw": map["rawName"],
+            "level": map["levelName"],
+            "map": map["mapName"],
+            "mode": map["gamemode"],
+            "layer": map["layerVersion"],
+            #plugin?
+            "size":  str(map["mapSize"]).split(" ")[0],
+            "sizeUnit": str(map["mapSize"]).split(" ")[1],
+            "weather": map["lighting"],
+            #commander can be different for each team // removed
+            "teams": teams,
+            "vehicles": map["vehicles"],
+            "flags": map["Flag"] if "Flag" in map else [],
+        }
+
+if __name__ == "__main__":
+    main()
+
